098c29a61caf874c777917ac761da03058fcff48
Dzmitry Filipuk <dzmitry@filipuk.me>
2019-09-01 17:05:04 +0300

    added scripts for building project

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+SETLOCAL
+
+CALL config.bat
+
+IF NOT EXIST .\bin\ MD bin
+%_fasm% .\Supersticks.asm .\bin\Sticks.com
+IF NOT %ERRORLEVEL% EQU 0 GOTO end
+
+IF NOT -%1-==-prod- GOTO end
+%_7zip% a -tzip -ssw -mx5 .\bin\Sticks.zip .\bin\Sticks.com
+
+:end
