54b95d074bc054c5c24f72b29bb796c04d95dfee
Dzmitry Filipuk <dzmitry@filipuk.me>
2019-09-02 21:04:52 +0300

    fixed typo

--- System/Keyboard/Keyboard.private.asm
+++ System/Keyboard/Keyboard.private.asm
@@ -15,6 +15,6 @@ Keyboard.Private.ClearInputBufferAndReadCharacter:
 ;   If result equals zero then extended ASCII input acquired.
 ;   Should be called second time to retreive extended input
 Keyboard.Private.ReadCharacter:
-    mov ah, 0C08h
+    mov ah, 08h
     int 21h
     ret
