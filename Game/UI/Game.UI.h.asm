6279d69f776d9e3f0c051e82d199725f8a552a6c
Dzmitry Filipuk <dzmitry@filipuk.me>
2019-09-20 22:59:17 +0300

    calls for sticks drawing init

--- Game/UI/Game.UI.h.asm
+++ Game/UI/Game.UI.h.asm
@@ -1,2 +1,6 @@
 include 'System\Display\Display.h.asm'
 include 'System\Keyboard\Keyboard.h.asm'
+
+Game.UI.STICK_HEIGHT = 50
+Game.UI.STICK_WIDTH = 3
+Game.UI.STICKS_GAP = Game.UI.STICK_WIDTH * 2
--- Game/UI/Game.UI.public.asm
+++ Game/UI/Game.UI.public.asm
@@ -1,11 +1,33 @@
 Game.UI.Public.Initialize:
     call Display.Public.Initialize
     call Display.Public.Clear
+
+    push Game.UI.STICKS_GAP
+    push Game.UI.STICK_WIDTH
+    push Game.UI.STICK_HEIGHT
+    call Display.Public.ConfigureSticksSize
     ret
 
 Game.UI.Public.Finalize:
     call Display.Public.Finalize
     ret
+
+; Parameters
+;   Stack1 -- Maximum sticks count
+; Returns
+;   None 
+Game.UI.Public.ConfigureGameScreen:
+    push bp
+    mov bp, sp
+    push ax
+
+    mov ax, [bp + 4]
+    push ax
+    call Display.Public.ConfigureSticksPosition
+
+    pop ax
+    pop bp
+    ret 2
 
 include 'Game\UI\Game.UI.private.asm'
 
