9781f952160f2d170df3344669288337c113dbd7
Dzmitry Filipuk <dzmitry@filipuk.me>
2019-11-30 23:12:08 +0300

    fixed function for reading keyboard input

--- System/Keyboard/Keyboard.public.asm
+++ System/Keyboard/Keyboard.public.asm
@@ -5,20 +5,10 @@ include 'System\Keyboard\Keyboard.private.asm'
 ; Returns
 ;   AX -- Read key
 Keyboard.Public.ReadKey:
-    push dx
-
-    xor dx, dx
     call Keyboard.Private.ClearInputBufferAndReadCharacter
-    mov dl, al
-
     test al, al
     jnz @F
-
     call Keyboard.Private.ReadCharacter
-    mov dh, al
-
 @@:
-    mov ax, dx
-
-    pop dx
+    xor ah, ah
     ret
