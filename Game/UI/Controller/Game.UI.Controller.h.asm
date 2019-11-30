6f575b668067c30af84d5f4549dc121d4e2e41cf
Dzmitry Filipuk <dzmitry@filipuk.me>
2019-11-30 21:26:52 +0300

    implemented getting user input for selection from list

--- Game/UI/Controller/Game.UI.Controller.h.asm
+++ Game/UI/Controller/Game.UI.Controller.h.asm
@@ -1,1 +1,6 @@
 include 'System\Keyboard\Keyboard.h.asm'
+
+Game.UI.Controller.SUBMIT = 0
+Game.UI.Controller.CANCEL = 1
+Game.UI.Controller.SELECT_NEXT = 2
+Game.UI.Controller.SELECT_PREVIOUS = 3
--- Game/UI/Controller/Game.UI.Controller.public.asm
+++ Game/UI/Controller/Game.UI.Controller.public.asm
@@ -1,2 +1,38 @@
 include 'System\Keyboard\Keyboard.public.asm'
 include 'Game\UI\Controller\Game.UI.Controller.private.asm'
+
+; Parameters
+;   None
+; Returns
+;   AX -- User action
+Game.UI.Controller.Public.GetUserInputForSelectionFromList:
+    .InputLoopStart:
+        call Keyboard.Public.ReadKey
+
+        cmp ax, Keyboard.VK_RETURN
+        jne @F
+        mov ax, Game.UI.Controller.SUBMIT
+        jmp .InputLoopEnd
+
+    @@:
+        cmp ax, Keyboard.VK_ESCAPE
+        jne @F
+        mov ax, Game.UI.Controller.CANCEL
+        jmp .InputLoopEnd
+
+    @@:
+        cmp ax, Keyboard.VK_UP
+        jne @F
+        mov ax, Game.UI.Controller.SELECT_PREVIOUS
+        jmp .InputLoopEnd
+
+    @@:
+        cmp ax, Keyboard.VK_DOWN
+        jne @F
+        mov ax, Game.UI.Controller.SELECT_NEXT
+        jmp .InputLoopEnd
+
+        jmp .InputLoopStart
+    .InputLoopEnd:
+
+    ret
